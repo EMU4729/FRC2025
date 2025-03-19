520aa0d69e593304af7d2149c3aea513c6a369ca
zodaz0 <lucaforzisi@outlook.com>
2025-03-19 19:31:54 +1100

    removed old rate limiter and added new ClosedSlewRateLimiter for the acceleration improvement

--- src/main/java/frc/robot/teleop/TeleopDriveSwerve.java
+++ src/main/java/frc/robot/teleop/TeleopDriveSwerve.java
@@ -5,6 +5,8 @@ import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RadiansPerSecondPerSecond;
 
+import com.fasterxml.jackson.databind.ser.std.ClassSerializer;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -15,21 +17,22 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.OI;
 import frc.robot.Subsystems;
 import frc.robot.constants.DriveConstants;
+import frc.robot.utils.ClosedSlewRateLimiter;
 import frc.robot.utils.SpeedRateLimiter;
 import frc.robot.utils.RangeMath.DriveBaseFit;
 
 public class TeleopDriveSwerve extends Command {
   private final DriveBaseFit settings;
 
-  private final SlewRateLimiter xLimiter = new SpeedRateLimiter(
+  private final ClosedSlewRateLimiter xLimiter = new ClosedSlewRateLimiter(
       DriveConstants.MAX_ACCELERATION.in(MetersPerSecondPerSecond),
-      -DriveConstants.MAX_DECELERATION.in(MetersPerSecondPerSecond), 0);
-  private final SlewRateLimiter yLimiter = new SpeedRateLimiter(
+      -DriveConstants.MAX_DECELERATION.in(MetersPerSecondPerSecond));
+  private final ClosedSlewRateLimiter yLimiter = new ClosedSlewRateLimiter(
       DriveConstants.MAX_ACCELERATION.in(MetersPerSecondPerSecond),
-      -DriveConstants.MAX_DECELERATION.in(MetersPerSecondPerSecond), 0);
-  private final SlewRateLimiter rLimiter = new SpeedRateLimiter(
+      -DriveConstants.MAX_DECELERATION.in(MetersPerSecondPerSecond));
+  private final ClosedSlewRateLimiter rLimiter = new ClosedSlewRateLimiter(
       DriveConstants.MAX_ANGULAR_ACCELERATION.in(RadiansPerSecondPerSecond),
-      -DriveConstants.MAX_ANGULAR_DECELERATION.in(RadiansPerSecondPerSecond), 0);
+      -DriveConstants.MAX_ANGULAR_DECELERATION.in(RadiansPerSecondPerSecond));
 
   private Rotation2d targetYaw = new Rotation2d(0);
 
@@ -58,13 +61,15 @@ public class TeleopDriveSwerve extends Command {
         booster,
         limiter);
 
-    var x = control[0];
-    var y = control[1];
-    var r = control[2];
+    var x = control[0] * DriveConstants.MAX_SPEED.in(MetersPerSecond);
+    var y = control[1] * DriveConstants.MAX_SPEED.in(MetersPerSecond);
+    var r = control[2] * DriveConstants.MAX_ANGULAR_SPEED.in(RadiansPerSecond);
 
-    x = xLimiter.calculate(x) * DriveConstants.MAX_SPEED.in(MetersPerSecond);
-    y = yLimiter.calculate(y) * DriveConstants.MAX_SPEED.in(MetersPerSecond);
-    r = rLimiter.calculate(r) * DriveConstants.MAX_ANGULAR_SPEED.in(RadiansPerSecond);
+    final var currentSpeeds = Subsystems.nav.getChassisSpeeds();
+
+    x = xLimiter.calculate(x, currentSpeeds.vxMetersPerSecond);
+    y = yLimiter.calculate(y, currentSpeeds.vyMetersPerSecond) ;
+    r = rLimiter.calculate(r, currentSpeeds.omegaRadiansPerSecond);
 
     // if we are on the red alliance and driving field-relative, we should invert
     // driver inputs since the field's origin is taken from the blue alliance
---
+++ src/main/java/frc/robot/utils/ClosedSlewRateLimiter.java
@@ -0,0 +1,24 @@
+package frc.robot.utils;
+
+public class ClosedSlewRateLimiter {
+  private final double positiveRate;
+  private final double negativeRate;
+
+  public ClosedSlewRateLimiter(double rate) {
+    this(rate, rate);
+  }
+
+  public ClosedSlewRateLimiter(double positiveRate, double negativeRate) {
+    this.positiveRate = positiveRate;
+    this.negativeRate = negativeRate;
+  }
+
+  public double calculate(double desired, double current) {
+    if (desired > current) {
+      return Math.min(current + positiveRate, desired);
+    } else {
+      return Math.max(current - negativeRate, desired);
+    }
+  }
+  
+}
