5d4eded3b004ed56f48c6abb1b79d88a1df427a4
roterrex <joel.roterex@gmail.com>
2025-06-16 18:46:11 +1000

    autos with no driving should not reset odom (crashes due to missing location)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,10 +10,12 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.LEDs.BatteryPercentLEDCommand;
 import frc.robot.LEDs.ClearLEDCommand;
 import frc.robot.LEDs.RainbowLEDCommand;
 import frc.robot.LEDs.TeamColorLEDCommand;
+import frc.robot.subsystems.DriveSub;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -132,6 +134,8 @@ public class Robot extends TimedRobot {
     CommandScheduler.getInstance().cancelAll();
     System.out
         .println("Test Start --------------------------------------------------------------------------------------");
+
+    Subsystems.drive.testFunction().schedule();
   }
 
   /** This function is called periodically during test mode. */
