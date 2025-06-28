81c5bb180861e4962f16463aee71f5764a559d89
Rias296 <tanamaefrain@gmail.com>
2025-06-28 16:49:19 +1000

    delete outatke coral dire

--- src/main/java/frc/robot/CoralOutake/CoralOutakeDriveToPosition.java
+++ src/main/java/frc/robot/CoralOutake/CoralOutakeDriveToPosition.java
@@ -11,42 +11,7 @@ import frc.robot.Subsystems;
 import frc.robot.constants.DriveConstants;
 
 public class CoralOutakeDriveToPosition extends Command {
-  private final Translation2d reefCentre = new Translation2d(13.1, 4);
-  private final double reefRadius = 1.4; 
-  private Pose2d targetLocation;
-  private double leftRightOffset = 0;
-
-  private Command pathCommand;
-  public CoralOutakeDriveToPosition(double leftRightOffset){
-    this.leftRightOffset = leftRightOffset;
-}
-
-public void initialize() {
-    targetLocation = closestReefSide();
-    pathCommand = AutoBuilder.pathfindToPose(targetLocation, DriveConstants.PATH_CONSTRAINTS);
-    pathCommand.schedule();
-  }
-  @Override
-  public boolean isFinished() {
-    return !pathCommand.isScheduled();
-  }
-
-  @Override
-  public void end(boolean interrupted) {
-    if(interrupted){pathCommand.cancel();}
-  }
-
-  public void DriveToPosition(double x, double y, double angle){
-    ChassisSpeeds speeds = new ChassisSpeeds(x,y,angle);
-    Subsystems.drive.drive(speeds, isScheduled());
-
-  }
-
-  public void CoordinateMagicNearest(){
-    Translation2d curLoc = Subsystems.nav.getPose().getTranslation();
-
-    
-  }
+ 
 
   
 
