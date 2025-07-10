bb270219f64472335ab475f3e5903954399662a1
unswfrc <unswfrcmedia@gmail.com>
2025-07-11 02:53:52 +1000

    make elevator work

--- src/main/java/frc/robot/constants/ElevatorConstants.java
+++ src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -15,23 +15,24 @@ public class ElevatorConstants {
   public static final EncoderSupplier ENCODER_ID = new EncoderSupplier(new int[] { 8, 9 }, 0.00003896311201);
   public static final Distance GROUND_TO_ELEVATOR = Meters.of(0.55);
 
-  public static final double CONTROLLER_P = 3;
-  public static final double CONTROLLER_I = 1;
+  public static final double CONTROLLER_P = 4;
+  public static final double CONTROLLER_I = 0;
   public static final double CONTROLLER_D = 0;
   public static final TrapezoidProfile.Constraints MOTION_CONSTRAINTS = new TrapezoidProfile.Constraints(1, 1);
   public static final double POSITION_TOLERANCE = 0.02;
 
-  public static final Distance MAX_ALLOWABLE_POSITION = Meters.of(0.78);
+  public static final Distance MAX_ALLOWABLE_POSITION = Meters.of(0.55);
 
   public static class ElevatorStops {
+    // TODO: tune these at field calibration
     public static final Distance INTAKE = Meters.of(0);
-    public static final Distance L1 = Meters.of(0.28);
-    public static final Distance L2 = Meters.of(0.4);
-    public static final Distance L3 = Meters.of(0.78);
+    public static final Distance L1 = Meters.of(0.15);
+    public static final Distance L2 = Meters.of(0.3);
+    public static final Distance L3 = Meters.of(0.5);
   }
 
   // ((1m) / (circumference of pulley)) * (gear ratio)
-  public static final double ENCODER_RATIO = (1000 / 27.666 * Math.PI) * 25;
+  public static final double ENCODER_RATIO = (1000 / (27.666 * Math.PI)) * 5;
 
   public static final FalconMotorSupplier LEFT_MOTOR_ID = new FalconMotorSupplier(7)
       .withEncoder(ENCODER_RATIO)
--- src/main/java/frc/robot/subsystems/ElevatorSub.java
+++ src/main/java/frc/robot/subsystems/ElevatorSub.java
@@ -2,20 +2,19 @@ package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.Meters;
 
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.units.measure.Distance;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.ElevatorConstants;
 
 public class ElevatorSub extends SubsystemBase {
   private final TalonFX motorLeft = ElevatorConstants.LEFT_MOTOR_ID.get();
-  private final TalonFX motorRight = ElevatorConstants.LEFT_MOTOR_ID.get();
+  private final TalonFX motorRight = ElevatorConstants.RIGHT_MOTOR_ID.get();
 
-  private final Encoder encoder = ElevatorConstants.ENCODER_ID.get();
   private final ProfiledPIDController controller = new ProfiledPIDController(
       ElevatorConstants.CONTROLLER_P,
       ElevatorConstants.CONTROLLER_I,
@@ -26,15 +25,16 @@ public class ElevatorSub extends SubsystemBase {
   private boolean disableEStop = false;
 
   public ElevatorSub() {
+    motorLeft.setPosition(0);
+    motorRight.setPosition(0);
+
+    motorRight.setControl(new Follower(motorLeft.getDeviceID(), true));
+
     controller.setIntegratorRange(-0.2, 0.2);
     controller.setIZone(0.05);
     controller.setTolerance(ElevatorConstants.POSITION_TOLERANCE);
 
-    motorLeft.setPosition(encoder.getDistance());
-    motorRight.setPosition(encoder.getDistance());
-
     SmartDashboard.putData("Elevator Motor", motorLeft);
-    SmartDashboard.putData("Elevator Encoder", encoder);
     SmartDashboard.putData("Elevator Controller", controller);
     SmartDashboard.putNumber("targetHeight", 0);
     SmartDashboard.putBoolean("Elevator E-Stopped", eStopped != EStopState.NONE);
@@ -67,7 +67,7 @@ public class ElevatorSub extends SubsystemBase {
 
     SmartDashboard.putNumber("motor pos", position.in(Meters));
 
-    if (motorLeftPosition.lt(Meters.of(0)) || motorRightPosition.lt(Meters.of(0))) {
+    if (motorLeftPosition.lt(Meters.of(0.02)) || motorRightPosition.lt(Meters.of(0.02))) {
       return EStopState.BOTTOM;
     }
 
@@ -83,11 +83,14 @@ public class ElevatorSub extends SubsystemBase {
   public void periodic() {
     SmartDashboard.putBoolean("Elevator E-Stopped", eStopped != EStopState.NONE);
     SmartDashboard.putString("Elevator E-Stop State", eStopped.toString());
+    SmartDashboard.putNumber("Elevator Distance", getPosition().in(Meters));
+    SmartDashboard.putNumber("Elevator Left Dist", motorLeft.getPosition().getValueAsDouble());
+    SmartDashboard.putNumber("Elevator Right Dist", motorRight.getPosition().getValueAsDouble());
 
     var out = 0.0d;
     if (!atTargetPosition()) {
       final var position = getPosition();
-      out = controller.calculate(position.in(Meters)) + 0.1;
+      out = controller.calculate(position.in(Meters));// + 0.1;
       out = MathUtil.clamp(out, -1, 1);
     } else {
 
@@ -118,7 +121,6 @@ public class ElevatorSub extends SubsystemBase {
 
       if (preventMove) {
         motorLeft.set(0);
-        motorRight.set(0);
         return;
       } else if (slowMove) {
         out = MathUtil.clamp(out, -0.1, 0.1);
@@ -126,7 +128,6 @@ public class ElevatorSub extends SubsystemBase {
     }
 
     motorLeft.set(out);
-    motorRight.set(out);
 
     SmartDashboard.putNumber("power", out);
     SmartDashboard.putNumber("targetHeight", controller.getGoal().position);
