b19f24cb50b35c70ba94e5f738bb4eda04342e45
roterrex <joel.roterex@gmail.com>
2025-09-06 23:28:04 +1000

    working on a better drive angle optimiser

--- src/main/java/frc/robot/utils/LibUpgrades/SwerveModuleStateUpgrade.java
+++ src/main/java/frc/robot/utils/LibUpgrades/SwerveModuleStateUpgrade.java
@@ -1,18 +1,25 @@
 package frc.robot.utils.LibUpgrades;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
 import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecondPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularAcceleration;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.LinearVelocity;
 
 public class SwerveModuleStateUpgrade extends SwerveModuleState {
+  private boolean optimised_last_loop = false;
   
   /** Constructs a SwerveModuleState with zeros for speed and angle. */
   public SwerveModuleStateUpgrade() {}
@@ -68,12 +75,51 @@ public class SwerveModuleStateUpgrade extends SwerveModuleState {
     return Radians.of(this.angle.getRadians());
   }
   
-
   @Override
-  public void optimize(Rotation2d currentAngle){
-    //TODO impliment
+  public void optimize(Rotation2d angle){
+
+
+    //throw new UnsupportedOperationException("Do Not Use : optimize(Angle currentAngle, AngularVelocity speed, AngularAcceleration maxAcceleration, AngularVelocity maxSpeed) is better");
   }
-  public void optimize(Angle currentAngle){
-    optimize(new Rotation2d(currentAngle));
+
+  public void optimize(Angle currentAngle, AngularVelocity currentVelocity, AngularVelocity maxVelocity, AngularAcceleration maxAcceleration){
+    //there is a big simplification here
+    //we expect acceleration to be high
+    //high enough to assume it is near infinite
+    //is this right no, but the time difference is also almost nothing
+    //so it is close enough
+
+    currentVelocity = RadiansPerSecond.of(Math.copySign(maxVelocity.in(RadiansPerSecond), currentVelocity.in(RadiansPerSecond)));
+
+    double d1 = currentAngle.minus(getAngle()).in(Radians);
+    double d2 = currentAngle.minus(Radians.of(Math.PI)).minus(getAngle()).in(Radians);
+    d1 += d1 < -Math.PI ? 2*Math.PI : (d1 > Math.PI ? -2*Math.PI : 0);
+    d2 += d2 < -Math.PI ? 2*Math.PI : (d2 > Math.PI ? -2*Math.PI : 0);  
+    
+    double t1 = Math.abs(d1 / currentVelocity.in(RadiansPerSecond));
+    double t2 = Math.abs(d2 / currentVelocity.in(RadiansPerSecond));
+
+    double t_inv = 2*Math.abs(currentVelocity.in(RadiansPerSecond)) / maxAcceleration.in(RadiansPerSecondPerSecond);
+
+    if (Math.signum(d1) == Math.signum(currentVelocity.in(RadiansPerSecond))){
+      t2 += t_inv;
+    } else {
+      t1 += t_inv;
+    }
+
+    if(optimised_last_loop){
+      t1 *= 1.05;
+    } else {
+      t2 *= 1.05;
+    }
+
+    if(t1 > t2){
+      optimised_last_loop = true;
+      speedMetersPerSecond *= -1;
+      angle = angle.plus(Rotation2d.k180deg);
+    } else {
+      optimised_last_loop = false;
+    }
+    System.out.println(d1+" "+d2+" "+t1+" "+t2+" "+optimised_last_loop);
   }
 }
